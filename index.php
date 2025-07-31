16fb535f5114f573a53c1e79d20f31941cad358d
petraalpigiano <petraalpigiano@gmail.com>
2025-07-31 18:17:22 +0200

    add cards

--- index.php
+++ index.php
@@ -5,30 +5,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
 
 <body>
     <?php
     $json_string = file_get_contents('./musicRecords.json');
     $musicRecords = json_decode($json_string, true);
-    echo "<pre>";
-    var_dump($musicRecords);
-    echo "</pre>";
-    $ciao = "ciao"
+    // echo "<pre>";
+    // var_dump($musicRecords);
+    // echo "</pre>";
     ?>
-    <h1><?php echo $ciao ?></h1>
-    <ul>
-        <?php
-        foreach ($musicRecords as $currentMusicRecord) {
-            foreach ($currentMusicRecord as $key => $value) {
-                echo "<li>$key : $value</li>";
+
+    <div class="container">
+        <h1 class="text-center my-3">Dischi</h1>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <?php
+            foreach ($musicRecords as $currentMusicRecord) {
+
+                echo "<div class='col'> 
+                        <div class='card h-100'>
+                        <img src='{$currentMusicRecord['cover']}' class='card-img-top' alt='...'>
+                        <div class='card-body'>
+                            <h5 class='card-title'>{$currentMusicRecord['title']}</h5>
+                            <p class='card-text'>{$currentMusicRecord['artist']}</p>
+                            <p class='card-text'>{$currentMusicRecord['genre']}</p>
+                            <p class='card-text'>{$currentMusicRecord['year']}</p>
+                        </div>
+                    </div>
+                </div>";
             }
-        }
-        ?>
-
-
-    </ul>
-
+            ?>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
 </body>
 
 </html>
