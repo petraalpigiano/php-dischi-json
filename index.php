73b45041616b7b5621b31240806e4b10bb13f8e2
petraalpigiano <petraalpigiano@gmail.com>
2025-07-31 12:35:14 +0200

    add json decode

--- index.php
+++ index.php
@@ -8,6 +8,26 @@
 </head>
 
 <body>
+    <?php
+    $json_string = file_get_contents('./musicRecords.json');
+    $musicRecords = json_decode($json_string, true);
+    echo "<pre>";
+    var_dump($musicRecords);
+    echo "</pre>";
+    $ciao = "ciao"
+    ?>
+    <h1><?php echo $ciao ?></h1>
+    <ul>
+        <?php
+        foreach ($musicRecords as $currentMusicRecord) {
+            foreach ($currentMusicRecord as $key => $value) {
+                echo "<li>$key : $value</li>";
+            }
+        }
+        ?>
+
+
+    </ul>
 
 </body>
 
